146ca7a8e3eb09a8d18428b08a5572faa92326fc
strangeal <bajerijustice@gmail.com>
2023-08-13 15:02:45 +0000

    Add `fetchData` component

---
+++ app/components/FetchData.tsx
@@ -0,0 +1,11 @@
+const FetchData = async (url: any) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default FetchData;
