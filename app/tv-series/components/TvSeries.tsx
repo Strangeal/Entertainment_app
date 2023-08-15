ec4b148014cd587405b46b09913608d5e203b881
strangeal <bajerijustice@gmail.com>
2023-08-15 10:34:45 +0000

    Modify `TvSeries` component to handle user search query

--- app/tv-series/components/TvSeries.tsx
+++ app/tv-series/components/TvSeries.tsx
@@ -4,10 +4,12 @@ import React, { useEffect, useState } from "react";
 import SeriesCard from "../page";
 import axios from "axios";
 import Card from "@/app/components/Card";
+import Search from "@/app/components/Search";
 
 type Props = {};
 
 const TvSeries = (props: Props) => {
+  const [searchQuery, setSearchQuery] = useState("");
   const [series, setSeries] = useState([]);
 
   useEffect(() => {
@@ -24,13 +26,29 @@ const TvSeries = (props: Props) => {
   }, []);
   console.log(series);
 
+  const handleSearch = (searchResult: any) => {
+    setSearchQuery(searchResult);
+  };
+
+  const filteredSearch = series.filter(
+    (movie: any) =>
+      !searchQuery ||
+      movie.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   return (
     <>
-      <h2>TvSeries</h2>
-      <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8">
-        {series &&
-          series.map((series, index) => <Card key={index} movie={series} />)}
-      </div>
+      <Search searchText="Tv-series" onSearch={handleSearch} />
+      <h2 className="text-3xl font-light my-5">TV Series</h2>
+      {filteredSearch.length === 0 ? (
+        <h2 className="text-3xl font-light my-5">No series found</h2>
+      ) : (
+        <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8">
+          {series &&
+            series.map((series: any) => (
+              <Card key={series.id} movie={series} searchQuery={searchQuery} />
+            ))}
+        </div>
+      )}
     </>
   );
 };
