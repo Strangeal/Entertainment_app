be838959e65e1b7a7816ac536650349b7678cbe9
strangeal <bajerijustice@gmail.com>
2023-08-15 13:47:18 +0000

    Update `Trending` component with swiper

--- app/components/trending/Trending.tsx
+++ app/components/trending/Trending.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import FetchData from "../FetchData";
 import TrendingCard from "./TrendingCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Autoplay } from "swiper/modules";
 
 const Trending = () => {
   const [trending, setTrending] = useState([]);
@@ -17,10 +20,32 @@ const Trending = () => {
   }, []);
 
   return (
-    <div>
-      <h2 className="text-3xl my-5">Trending</h2>
-      {trending && trending.map((trend) => <TrendingCard trend={trend} />)}
-    </div>
+    <>
+      <h2 className="text-3xl font-light my-5">Trending</h2>
+      <Swiper
+        slidesPerView={1.5}
+        spaceBetween={10}
+        autoplay={{
+          delay: 2500,
+          pauseOnMouseEnter: true,
+        }}
+        breakpoints={{
+          900: {
+            slidesPerView: 2.6,
+            spaceBetween: 20,
+          },
+        }}
+        modules={[Autoplay]}
+        className="mySwiper mb-10"
+      >
+        {trending &&
+          trending.map((trend: any) => (
+            <SwiperSlide className="relative w-fit">
+              <TrendingCard key={trend.id} trend={trend} />
+            </SwiperSlide>
+          ))}
+      </Swiper>
+    </>
   );
 };
 
