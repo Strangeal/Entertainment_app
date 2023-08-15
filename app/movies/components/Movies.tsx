aa7271fd0538b5b3ae36b94d3abf249e4c6ff22a
strangeal <bajerijustice@gmail.com>
2023-08-15 10:23:20 +0000

    Import search component and user `search query`

--- app/movies/components/Movies.tsx
+++ app/movies/components/Movies.tsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import FetchData from "@/app/components/FetchData";
 import Card from "@/app/components/Card";
+import Search from "@/app/components/Search";
 
 const Movies = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -20,12 +22,28 @@ const Movies = () => {
     fetchMovies();
   }, []);
 
+  const handleSearch = (searchResult: any) => {
+    setSearchQuery(searchResult);
+  };
+
+  const filteredSearch = movies.filter(
+    (movie: any) =>
+      !searchQuery ||
+      movie.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   return (
     <>
-      <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8">
-        {movies &&
-          movies.map((movie: any, index) => <Card key={index} movie={movie} />)}
-      </div>
+      <Search searchText="Movies" onSearch={handleSearch} />
+      <h2 className="text-3xl font-light my-5">Movies</h2>
+      {filteredSearch.length === 0 ? (
+        <h2 className="text-3xl font-light my-5">No movies found</h2>
+      ) : (
+        <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8">
+          {filteredSearch.map((movie: any) => (
+            <Card key={movie.id} movie={movie} searchQuery={searchQuery} />
+          ))}
+        </div>
+      )}
     </>
   );
 };
