a358e421a7079da2b673fa445a118e9e77bbbc0a
strangeal <bajerijustice@gmail.com>
2023-08-15 10:24:09 +0000

    Clean up `Moviecard`

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,21 +1,10 @@
-import Search from "../components/Search";
 import Movies from "./components/Movies";
 
-type MovieProps = {
-  movie: any;
-};
-
-const MovieCard = ({ movie }: MovieProps) => {
-  console.log(movie);
-
+const MovieCard = () => {
   return (
-    <div>
-      <Search searchText="Movies" />
-      <h1>Movies Page</h1>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
-        <Movies />
-      </div>
-    </div>
+    <>
+      <Movies />
+    </>
   );
 };
 
