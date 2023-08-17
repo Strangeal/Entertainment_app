75dc879d04cbba40ad89aa9c4c1b83c5746fc759
strangeal <bajerijustice@gmail.com>
2023-08-17 20:25:38 +0000

    Pass isLoading state to fetch data component

--- app/components/fetch & filters/useFetchFilter.ts
+++ app/components/fetch & filters/useFetchFilter.ts
@@ -3,15 +3,23 @@ import FetchData from "./FetchData";
 
 type FilterCategoryProps = {
   filterData: any;
+  setIsLoading: any;
 };
 
-const useFetchFilter = ({ filterData }: FilterCategoryProps) => {
+const useFetchFilter = ({ filterData, setIsLoading }: FilterCategoryProps) => {
   const [data, setData] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const allData = await FetchData("http://localhost:3001/data");
-      const filteredData = allData.filter(filterData);
-      setData(filteredData);
+      try {
+        const allData = await FetchData(
+          "http://localhost:3001/data",
+          setIsLoading
+        );
+        const filteredData = allData.filter(filterData);
+        setData(filteredData);
+      } catch (error) {
+        console.log(error);
+      }
     };
     fetchData();
   }, []);
